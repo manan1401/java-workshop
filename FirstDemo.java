77b488de86b074600ff857f6656994433a6a20c6
mananmk <manankotak182000@gmail.com>
2020-02-05 10:17:04 +0530

    java

---
+++ FirstDemo.java
@@ -0,0 +1,12 @@
+
+public class FirstDemo {
+	void showData () {
+		System.out.printf (  "hello" );
+	}
+ public static void main (String [] args ) {
+	 FirstDemo demo=new FirstDemo ();
+	 demo.showData ();
+	 
+ }
+ 
+}
